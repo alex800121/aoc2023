41cf4a6ec77550ed4a673177b6545550a82d6739
alex800121 <alex800121@hotmail.com>
2023-12-06 16:21:51 +0800

    day6

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,6 +1,28 @@
 module Day6 where
+import Data.Bifunctor (Bifunctor(bimap))
+import Data.List.Split (splitOn)
 
+f :: (Floating a) => a -> a -> a -> (a -> a -> a) -> a
+f a b c x = (negate b `x` sqrt (b ^ 2 - (4 * a * c))) / (2 * a)
+
+g (x, y) = floor (f' (-)) - ceiling (f' (+)) + 1
+  where
+   f' = f (-1) x (-y)
+
+testInput = [(7, 9), (15, 40), (30, 200)]
+-- x * (7 - x)
+-- = -(x^2) + 7x
 day6 :: IO ()
 day6 = do
-  -- input <- readFile "input/input6.txt"
-  return ()
+  input <- lines  <$> readFile "input/input6.txt"
+  let day6a = product $ map g . (\(x : y : _) -> zip x y) . map (map read . tail . words) $ input
+      input' = (\(x : y : _) -> g (read x, read y)) $ map (filter (/= ' ') . last . splitOn ":") input
+      -- day6b = g input'
+  putStrLn
+    . ("day6a: " ++)
+    . show
+    $ day6a
+  putStrLn 
+    . ("day6b: " ++)
+    . show
+    $ input'
