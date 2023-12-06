2cfc5ff21d44e7118b0a3b3a6335c60ceca868e0
alex800121 <alex800121@hotmail.com>
2023-12-06 10:28:58 +0800

    day5

--- src/Day1.hs
+++ src/Day1.hs
@@ -4,27 +4,27 @@ import Data.Char (digitToInt, intToDigit, isDigit)
 import Data.List (find, inits, isPrefixOf, isSuffixOf, tails)
 import Data.Maybe (isJust, mapMaybe)
 
-digits =
-  (`zip` ([1 .. 9] ++ [1 .. 9]))
-    ( ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-        ++ [[intToDigit x] | x <- [1 .. 9]]
-    )
+digitsA = [[intToDigit x] | x <- [1 .. 9]] `zip` [1 .. 9]
 
-f :: String -> Int
-f s = 10 * x + y
+digitsB =
+  ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] `zip` ([1 .. 9] ++ [1 .. 9])
+
+-- f :: String -> Int
+f digits s = 10 * x + y
   where
-    x = head . mapMaybe (fmap snd . (\x -> find (\(a, b) -> a `isPrefixOf` x) digits)) $ tails s
-    y = head . mapMaybe (fmap snd . (\x -> find (\(a, b) -> a `isSuffixOf` x) digits)) $ reverse $ inits s
+    xy = mapMaybe (fmap snd . (\x -> find (\(a, b) -> a `isPrefixOf` x) digits)) $ tails s
+    x = head xy
+    y = last xy
 
 day1 :: IO ()
 day1 = do
   -- input <- lines <$> readFile "input/test1.txt"
   input <- lines <$> readFile "input/input1.txt"
-  let a = sum $ map (((+) <$> ((* 10) . head) <*> last) . map digitToInt . filter isDigit) input
-      b = sum $ map f input
-  putStrLn 
+  let a = sum $ map (f digitsA) input
+      b = sum $ map (f (digitsA ++ digitsB)) input
+  putStrLn
     . ("day1a: " ++)
     $ show a
-  putStrLn 
+  putStrLn
     . ("day1b: " ++)
     $ show b
