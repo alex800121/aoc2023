3b0266b62679e52338d80d9053810d6a7ef3f062
alex800121 <alex800121@hotmail.com>
2023-12-17 06:52:35 +0800

    day16a

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,15 +1,12 @@
 module Day12 where
 
-import Control.Monad (forM)
-import Control.Monad.Trans.State
+import Control.Monad.Trans.State.Strict (State, evalState, get, modify, modify')
+import qualified Data.Array as A
 import Data.Bifunctor (Bifunctor (..))
 import Data.Bits (Bits (..))
 import Data.List (inits, intercalate, tails, transpose, uncons)
 import Data.List.Split (splitOn)
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Debug.Trace (traceM, traceShow)
-import MyLib (toBits)
+import Debug.Trace (traceShow)
 
 inputParser :: String -> (String, [Int])
 inputParser s = (x, i)
@@ -17,37 +14,44 @@ inputParser s = (x, i)
     x : y : _ = words s
     i = map read $ splitOn "," y
 
-calc :: String -> [Int] -> State (Map (String, [Int]) Int) Int
-calc [] (x : _) = return 0
-calc s [] = if '#' `notElem` s then return 1 else return 0
-calc s l@(x : xs) =
-  let s' = dropWhile (== '.') s
-   in get >>= \m -> case m Map.!? (s', l) of
-        Just i -> return i
-        Nothing -> do
-          let ss =
-                map (drop 1 . snd)
-                  . filter (\(h, t) -> length h == x && notElem '.' h && maybe True ((/= '#') . fst) (uncons t))
-                  . map (splitAt x . snd)
-                  . takeWhile (notElem '#' . fst)
-                  . (zip <$> inits <*> tails)
-                  $ s'
-          i <- sum <$> forM ss (`calc` xs)
-          modify' (Map.insert (s', l) i . Map.insert (s, l) i)
-          return i
+type Cache = A.Array (Int, Int) Int
+
+calc :: String -> [Int] -> Int
+calc x y = calc' x y
+  where
+    lx = length x
+    ly = length y
+    calc' a b = cache A.! (lx - length a, ly - length b)
+    cache =
+      A.array
+        ((0, 0), (length x, length y))
+        [ ((a, b), n)
+          | a <- [0 .. lx],
+            b <- [0 .. ly],
+            let n = calc'' (drop a x) (drop b y)
+        ]
+    calc'' s [] = if '#' `elem` s then 0 else 1
+    calc'' [] (_ : _) = 0
+    calc'' ss'@(s : ss) ns'@(n : ns) = case s of
+      '.' -> calc' ss ns'
+      '#' ->
+        let (a, b) = splitAt n ss'
+         in if length a == n && '.' `notElem` a && maybe True ((/= '#') . fst) (uncons b)
+              then calc' (drop 1 b) ns
+              else 0
+      '?' -> calc' ss ns' + calc'' ('#' : ss) ns'
 
 day12 :: IO ()
 day12 = do
   input <- map inputParser . lines <$> readFile "input/input12.txt"
-  let day12a = runState (mapM (uncurry calc) input) Map.empty
-      day12b = runState (mapM (\(x, y) -> calc (intercalate "?" $ replicate 5 x) (concat $ replicate 5 y)) input) Map.empty
+  -- input <- map inputParser . lines <$> readFile "input/test12.txt"
   putStrLn
     . ("day12a: " ++)
     . show
     . sum
-    $ fst day12a
+    $ map (uncurry calc) input
   putStrLn
     . ("day12b: " ++)
     . show
     . sum
-    $ fst day12b
+    $ map (\(x, y) -> calc (intercalate "?" $ replicate 5 x) (concat $ replicate 5 y)) input
