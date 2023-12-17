b96bf4ddd6ebdae9fab77829fb19d9d3e0077f00
alex800121 <alex800121@hotmail.com>
2023-12-18 07:41:17 +0800

    break day16b

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day16 where
 
 import Data.Array (Array, Ix (..), bounds, (!))
+import qualified Data.Array as A
 import Data.Bifunctor (Bifunctor (bimap))
 import Data.Set (Set)
 import qualified Data.Set as Set
 import MyLib (Direction (..), drawArray)
+import Debug.Trace (traceShow)
 
 type Index = (Int, Int)
 
@@ -40,13 +44,50 @@ calcSingle a (i, d)
     d' = reflect (a ! i) d
     i' = map ((,) <$> (bimap (+ fst i) (+ snd i) . toIndex) <*> id) d'
 
-type Cache = Array (Index, Direction) Int
+type Cache = Array Beam (Set Beam)
 
-calcCache :: Array Index Char -> Set Beam -> Set Beam -> Int
-calcCache a = undefined
+calcCache :: Array Index Char -> Beam -> Set Beam
+calcCache a = getCache
+  where
+    getCache = (cache A.!)
+    b@((minX, minY), (maxX, maxY)) = bounds a
+    b' = bimap (,North) (,West) b
+    cache =
+      A.array
+        b'
+        [(i, calc' Set.empty (Set.singleton i)) | i <- range b']
+    calc' visited s
+      | Set.null s' = visited
+      | otherwise = Set.unions $ Set.map getCache  nexts
+      where
+        f x = Set.filter (inRange b . fst) x Set.\\ visited
+        s' = f s
+        visited' = Set.union visited s'
+        nexts = f $ Set.unions $ Set.map (calcSingle a) s'
+
+day16b :: Array Index Char -> Int
+day16b a = maximum $ map (length . Set.map fst . calcCache a) edges
+  where
+    ((minX, minY), (maxX, maxY)) = bounds a
+    edges =
+      [((minX, y), East) | y <- [minY .. maxY]]
+        ++ [((maxX, y), West) | y <- [minY .. maxY]]
+        ++ [((x, minY), South) | x <- [minX .. maxX]]
+        ++ [((x, maxY), North) | x <- [minX .. maxX]]
 
 day16 :: IO ()
 day16 = do
   input <- drawArray @Array . lines <$> readFile "input/input16.txt"
   -- input <- drawArray @Array . lines <$> readFile "input/test16.txt"
-  print $ length $ Set.map fst $ calc input Set.empty $ Set.singleton ((0, 0), East)
+  putStrLn
+    . ("day16a: " ++)
+    . show
+    . length
+    . Set.map fst
+    . calc input Set.empty
+    $ Set.singleton ((0, 0), East)
+  putStrLn
+    . ("day16b: " ++)
+    . show
+    $ day16b input
+  -- print $ range (West, South)
