83902cbec0164f106cf68f9e4d0266fab70e582e
alex800121 <alex800121@hotmail.com>
2023-12-20 07:55:03 +0800

    on day18

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,22 +1,38 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Redundant bracket" #-}
+{-# LANGUAGE LambdaCase #-}
 module Day18 where
 
-import Data.Colour.Word8
-import MyLib (Direction (..), Parser, signedInteger)
+import Data.Bifunctor (Bifunctor (..))
+import Data.Char (digitToInt)
+import Data.Ix (inRange)
+import Data.List (foldl', scanl')
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Debug.Trace
+import MyLib (Direction (..), Parser, drawGraph, signedInteger, toIndex)
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Data.List (foldl')
-import Data.Char (digitToInt)
 
-type DigPlan = (Direction, Int, Colour8)
+type DigPlan = (Colour8, Colour8)
+
+type Colour8 = (Direction, Int)
+
+type Index = (Int, Int)
+
+type Hole = Set Index
 
 readColour8 :: Parser Colour8
 readColour8 = do
   optional $ char '#'
   let f = fromIntegral . foldl' (\acc x -> digitToInt x + 16 * acc) 0
-  r <- f <$> count 2 hexDigitChar
-  g <- f <$> count 2 hexDigitChar
-  b <- f <$> count 2 hexDigitChar
-  return $ Colour8 r g b
+  r <- f <$> count 5 hexDigitChar
+  d <- (pred . toEnum . digitToInt) <$> anySingle
+  return (d, r)
 
 readInput :: Parser DigPlan
 readInput = do
@@ -25,10 +41,63 @@ readInput = do
   i <- signedInteger
   space
   c <- between (char '(') (char ')') readColour8
-  pure (d, i, c)
+  pure ((d, i), c)
 
+dig :: [Colour8] -> (Hole, Hole, Hole)
+dig = go Set.empty Set.empty Set.empty (0, 0)
+  where
+    go acc accR accL _ [] = (acc, accR, accL)
+    go acc accR accL (x, y) ((d, i) : xs)
+      | i <= 0 = go acc accR accL (x, y) xs
+      | otherwise = go (Set.insert i' acc) (Set.insert iR accR) (Set.insert iL accL) i' ((d, i - 1) : xs)
+      where
+        i' = bimap (x +) (y +) (toIndex d)
+        iR = bimap (fst i' +) (snd i' +) (toIndex (succ d))
+        iL = bimap (fst i' +) (snd i' +) (toIndex (pred d))
+
+
+countInterior :: Hole -> Hole -> Hole -> Set Index
+countInterior border r l = Set.union k $ Set.union (f Set.empty rk) (f Set.empty lk)
+  where
+    f visited start
+      -- \| traceShow (length start) False = undefined
+      | not (all (inRange b) start) = Set.empty
+      | Set.null start = visited
+      | otherwise = f visited' start'
+      where
+        start' = (Set.\\ visited) $ (Set.\\ k) $ Set.unions $ map (\(x, y) -> Set.map (bimap (+ x) (+ y)) start) [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visited' = Set.union visited start
+    rk = r Set.\\ k
+    lk = l Set.\\ k
+    k = border
+    xs = Set.map fst k
+    ys = Set.map snd k
+    b = ((minimum xs, minimum ys), (maximum xs, maximum ys))
+
+shoelace :: [Index] -> Int
+shoelace ((x0, y0) : (x1, y1) : xs) = (x0 * y1) - (x1 * y0) + shoelace ((x1, y1) : xs)
+shoelace _ = 0
+
+angles :: Floating a => [Colour8] -> [((a, a) -> (a, a), (a, a) -> (a, a))]
+angles ((d, i) : xs) = f (a, b) xs
+  where
+    g x i = if odd (fromEnum x) then undefined else undefined
+      where
+        (ia, ib) = bimap ((+ fst i) . (/ 2) . fromIntegral) ((+ snd i) . (/ 2) . fromIntegral) $ toIndex x
+    (a, b) = undefined
+    f = undefined
 day18 :: IO ()
 day18 = do
-  input <- map (parseMaybe readInput) . lines <$> readFile "input/input18.txt"
-  print input
-  parseTest readInput "R 6 (#70c710)"
+  input <- map (fromJust . parseMaybe readInput) . lines <$> readFile "input/test18.txt"
+  -- input <- map (fromJust . parseMaybe readInput) . lines <$> readFile "input/input18.txt"
+  let (hole, holeR, holeL) = dig $ map fst input
+  putStrLn
+    . ("day18a: " ++)
+    . show
+    . length
+    $ countInterior hole holeR holeL
+  putStrLn
+    . unlines
+    . drawGraph (\case Nothing -> ' '; Just _ -> '#')
+    . Map.fromSet (const ())
+    $ hole
--- src/Day19.hs
+++ src/Day19.hs
@@ -1,6 +1,187 @@
 module Day19 where
 
+import Control.Lens
+import Data.List (foldl')
+import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe, maybeToList)
+import Debug.Trace
+import MyLib hiding (S)
+import qualified MyLib as M
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+data XMAS a = XMAS {_getX :: a, _getM :: a, _getA :: a, _getS :: a}
+  deriving (Show, Eq, Ord)
+
+type XMASI = XMAS Int
+
+type XMASR = Vec (M.S (M.S (M.S (M.S Z)))) Range
+
+type Range = (Int, Int)
+
+data Field = X | M | A | S deriving (Show, Eq, Ord, Enum)
+
+defaultXMAS = XMAS 0 0 0 0
+
+data Clause = C Field Ordering Int Dest | D Dest deriving (Show, Eq, Ord)
+
+data Dest = Accept | Reject | Next {_getNext :: String} deriving (Show, Eq, Ord)
+
+type Rule = [Clause]
+
+type Rules = Map String Rule
+
+defaultXMASR :: XMASR
+defaultXMASR = pure (1, 4001)
+
+applyRuleR :: Rule -> XMASR -> Map Dest [XMASR]
+applyRuleR = f Map.empty
+  where
+    g x y = jointEucVecs (x <> y)
+    -- f x y z | trace (show z) False = undefined
+    -- f acc [] _ = acc
+    f acc [D d] v = Map.insertWith g d [v] acc
+    f acc (C fi o i d : rs) v = case v1 of
+      Just v' -> f acc' rs v'
+      Nothing -> acc'
+      -- Nothing -> trace ("done: " ++ show acc') acc'
+      where
+        acc' = Map.insertWith g d v0 acc
+        (v0, v1) = case o of
+          GT ->
+            ( maybeToList $ overlapEucVec v (writeFieldR defaultXMASR fi (i + 1, 4001)),
+              overlapEucVec v (writeFieldR defaultXMASR fi (1, i + 1))
+            )
+          LT ->
+            ( maybeToList $ overlapEucVec v (writeFieldR defaultXMASR fi (1, i)),
+              overlapEucVec v (writeFieldR defaultXMASR fi (i, 4001))
+            )
+
+applyRulesR :: Rules -> Map Dest [XMASR] -> Map Dest [XMASR]
+applyRulesR r acc
+  -- \| trace (show (Map.filter ((> 1) . length) acc) ++ "\n") False = undefined
+  -- \| acc' == Map.filter (not . null) (Map.map jointEucVecs acc'') = undefined
+  -- \| traceShow (Map.map calc acc) False = undefined
+  | all (`elem` [Accept, Reject]) (Map.keys acc') = acc'
+  | otherwise = applyRulesR r acc''
+  where
+    -- calc = sum . product . map (fmap (uncurry subtract))
+    g x y = x ++ y
+    acc' = Map.filter (not . null) acc
+    acc'' =
+      Map.foldlWithKey'
+        ( \b k a -> case k of
+            Next s ->
+              let rule = r Map.! s
+                  xs = map (applyRuleR rule) a
+               in Map.unionsWith g (b : xs)
+            _ -> Map.insertWith g k a b
+        )
+        Map.empty
+        acc'
+
+readFieldR :: (Enum x) => Vec n a -> x -> a
+readFieldR v = vRead v . fromEnum
+
+writeFieldR :: (Enum x) => Vec n a -> x -> a -> Vec n a
+writeFieldR v x = vWrite v (fromEnum x)
+
+modifyFieldR :: (Enum x) => Vec n a -> x -> (a -> a) -> Vec n a
+modifyFieldR v x = vModify v (fromEnum x)
+
+applyRule :: Rule -> XMASI -> Dest
+applyRule [] _ = undefined
+applyRule (D d : _) _ = d
+applyRule (C f o i d : rs) xmas =
+  if compare (readField f xmas) i == o then d else applyRule rs xmas
+
+applyRules :: Rules -> Dest -> XMASI -> Dest
+applyRules m (Next s) xmas = applyRules m (applyRule (m Map.! s) xmas) xmas
+applyRules _ d _ = d
+
+readField X = _getX
+readField M = _getM
+readField A = _getA
+readField S = _getS
+
+writeField X x m = m {_getX = x}
+writeField M x m = m {_getM = x}
+writeField A x m = m {_getA = x}
+writeField S x m = m {_getS = x}
+
+parseRules :: Parser Rules
+parseRules = do
+  name <- many letterChar
+  rules <-
+    between
+      (char '{')
+      (char '}')
+      (sepBy parseClause (char ','))
+  pure $ Map.singleton name rules
+
+parseClause :: Parser Clause
+parseClause = do
+  let f = do
+        x <-
+          (char 'x' >> pure X)
+            <|> (char 'm' >> pure M)
+            <|> (char 'a' >> pure A)
+            <|> (char 's' >> pure S)
+        b <- (char '>' >> pure GT) <|> (char '<' >> pure LT)
+        i <- signedInteger
+        char ':'
+        pure (x, b, i)
+  a <- optional (try f)
+  d <- (char 'A' >> pure Accept) <|> (char 'R' >> pure Reject) <|> Next <$> many letterChar
+  case a of
+    Just (x, b, i) -> pure $ C x b i d
+    Nothing -> pure $ D d
+
+parseXMAS :: Parser XMASI
+parseXMAS =
+  foldl' (&) defaultXMAS
+    <$> between
+      (char '{')
+      (char '}')
+      ( sepBy
+          ( parseXMASLens
+              >>= \f ->
+                char '='
+                  >> signedInteger
+                  >>= \x -> pure (writeField f x)
+          )
+          (char ',')
+      )
+
+parseXMASLens :: Parser Field
+parseXMASLens =
+  (char 'x' >> pure X)
+    <|> (char 'm' >> pure M)
+    <|> (char 'a' >> pure A)
+    <|> (char 's' >> pure S)
+
+calcXMAS :: XMASI -> Int
+calcXMAS x = sum $ map ($ x) [_getX, _getM, _getA, _getS]
+
 day19 :: IO ()
 day19 = do
-  -- input <- readFile "input/input19.txt"
-  return ()
+  -- x : y : _ <- map lines . splitOn "\n\n" <$> readFile "input/test19.txt"
+  x : y : _ <- map lines . splitOn "\n\n" <$> readFile "input/input19.txt"
+  let rules = Map.unions $ mapMaybe (parseMaybe parseRules) x
+      xmas = mapMaybe (parseMaybe parseXMAS) y
+  putStrLn
+    . ("day19a: " ++)
+    . show
+    . sum
+    . map calcXMAS
+    $ filter ((== Accept) . applyRules rules (Next "in")) xmas
+  putStrLn
+    . ("day19b: " ++)
+    . show
+    . sum
+    . map (product . fmap (uncurry subtract))
+    . (Map.! Accept)
+    . applyRulesR rules
+    $ Map.singleton (Next "in") [defaultXMASR]
