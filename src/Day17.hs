4ea617fe34d53f4aec5357fc2ee148b3320cff92
alex800121 <alex800121@hotmail.com>
2023-12-20 21:44:30 +0800

    on day20

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,6 +1,56 @@
 module Day17 where
 
+import qualified Data.Array.Unboxed as U
+import Data.Bifunctor (Bifunctor (..))
+import Data.Char (digitToInt)
+import qualified Data.PQueue.Prio.Min as Q
+import Data.Set (Set)
+import qualified Data.Set as Set
+import MyLib
+
+data GameState = G
+  { _location :: Index,
+    _direction :: Direction,
+    _moves :: Int
+  }
+  deriving (Show, Ord, Eq)
+
+type Index = (Int, Int)
+
+type Q = Q.MinPQueue Int GameState
+
+type M = U.UArray Index Int
+
+dijkstra :: M -> Index -> Set GameState -> Q -> Maybe Int
+dijkstra m end visited q = case q of
+  Q.Empty -> Nothing
+  (k, g) Q.:< _ | _location g == end -> Just k
+  (_, g) Q.:< xs | g `Set.member` visited -> dijkstra m end visited xs
+  (k, g@(G location direction moves)) Q.:< xs ->
+    let visited' = Set.insert g visited
+        added =
+          Q.fromList $
+            [ (k', G l' d' m')
+              | let m' = 1,
+                d' <- map ($ direction) [succ, pred],
+                let l' = bimap (+ fst location) (+ snd location) $ toIndex d',
+                U.inRange b l',
+                let k' = k + m U.! l'
+            ] ++
+            [ (k', G l' direction m')
+              | let m' = moves + 1,
+                m' <= 3,
+                let l' = bimap (+ fst location) (+ snd location) $ toIndex direction,
+                U.inRange b l',
+                let k' = k + m U.! l'
+            ]
+        xs' = Q.union xs added
+     in dijkstra m end visited' xs'
+  where
+    b = U.bounds m
+
 day17 :: IO ()
 day17 = do
-  -- input <- readFile "input/input17.txt"
-  return ()
+  -- input <- U.amap digitToInt . drawArray @U.UArray . lines <$> readFile "input/test17.txt"
+  input <- U.amap digitToInt . drawArray @U.UArray . lines <$> readFile "input/input17.txt"
+  print $ dijkstra input (snd $ U.bounds input) Set.empty (Q.singleton 0 (G (0, 0) East 0))
--- src/Day18.hs
+++ src/Day18.hs
@@ -1,11 +1,10 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Redundant bracket" #-}
 {-# LANGUAGE LambdaCase #-}
+
 module Day18 where
 
 import Data.Bifunctor (Bifunctor (..))
 import Data.Char (digitToInt)
+import Data.Function (on)
 import Data.Ix (inRange)
 import Data.List (foldl', scanl')
 import Data.Map (Map)
@@ -55,7 +54,6 @@ dig = go Set.empty Set.empty Set.empty (0, 0)
         iR = bimap (fst i' +) (snd i' +) (toIndex (succ d))
         iL = bimap (fst i' +) (snd i' +) (toIndex (pred d))
 
-
 countInterior :: Hole -> Hole -> Hole -> Set Index
 countInterior border r l = Set.union k $ Set.union (f Set.empty rk) (f Set.empty lk)
   where
@@ -74,30 +72,52 @@ countInterior border r l = Set.union k $ Set.union (f Set.empty rk) (f Set.empty
     ys = Set.map snd k
     b = ((minimum xs, minimum ys), (maximum xs, maximum ys))
 
-shoelace :: [Index] -> Int
-shoelace ((x0, y0) : (x1, y1) : xs) = (x0 * y1) - (x1 * y0) + shoelace ((x1, y1) : xs)
-shoelace _ = 0
-
-angles :: Floating a => [Colour8] -> [((a, a) -> (a, a), (a, a) -> (a, a))]
-angles ((d, i) : xs) = f (a, b) xs
+shoelace :: (Num a) => [(a, a)] -> a
+shoelace x = f (x ++ take 1 x)
   where
-    g x i = if odd (fromEnum x) then undefined else undefined
+    f ((x0, y0) : (x1, y1) : xs) = (x0 * y1) - (x1 * y0) + f ((x1, y1) : xs)
+    f _ = 0
+
+angles :: (Floating a) => [Colour8] -> [((a, a) -> (a, a), (a, a) -> (a, a), (a, a) -> (a, a))]
+angles = map (\(d, j) -> (a d j, g d succ j, g d pred j))
+  where
+    a d j = let (x, y) = toIndex d in bimap (+ fromIntegral (x * j)) (+ fromIntegral (y * j))
+    g :: (Floating a) => Direction -> (Direction -> Direction) -> Int -> (a, a) -> (a, a)
+    g x y j i =
+      if odd (fromEnum x)
+        then second (const ib) i
+        else first (const ia) i
       where
-        (ia, ib) = bimap ((+ fst i) . (/ 2) . fromIntegral) ((+ snd i) . (/ 2) . fromIntegral) $ toIndex x
-    (a, b) = undefined
-    f = undefined
+        (ia, ib) = bimap ((+ fst i) . (/ 2) . fromIntegral) ((+ snd i) . (/ 2) . fromIntegral) $ toIndex $ y x
+
+walk :: (Floating a) => [Colour8] -> [((a, a), (a, a))]
+walk = f (0.5, 0.5) . angles
+  where
+    f i ((f0, f1, f2) : g@(g0, g1, g2) : xs) = (g1 (f1 i'), g2 (f2 i')) : f i' (g : xs)
+      where
+        i' = f0 i
+    f _ _ = []
+
 day18 :: IO ()
 day18 = do
-  input <- map (fromJust . parseMaybe readInput) . lines <$> readFile "input/test18.txt"
-  -- input <- map (fromJust . parseMaybe readInput) . lines <$> readFile "input/input18.txt"
+  -- input <- map (fromJust . parseMaybe readInput) . lines <$> readFile "input/test18.txt"
+  input <- map (fromJust . parseMaybe readInput) . lines <$> readFile "input/input18.txt"
   let (hole, holeR, holeL) = dig $ map fst input
+      frl = angles $ map snd input
+      f = round . (/ 2) . abs . shoelace
   putStrLn
     . ("day18a: " ++)
     . show
-    . length
-    $ countInterior hole holeR holeL
+    . uncurry max
+    . bimap f f
+    . unzip
+    . walk
+    $ map fst (input ++ take 1 input)
   putStrLn
-    . unlines
-    . drawGraph (\case Nothing -> ' '; Just _ -> '#')
-    . Map.fromSet (const ())
-    $ hole
+    . ("day18b: " ++)
+    . show
+    . uncurry max
+    . bimap f f
+    . unzip
+    . walk
+    $ map snd (input ++ take 1 input)
