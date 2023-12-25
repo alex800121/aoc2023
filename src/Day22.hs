55208add44f468b080a0d405fc129ce926796e1b
alex800121 <alex800121@hotmail.com>
2023-12-26 04:32:03 +0800

    on day23

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,6 +1,9 @@
 module Day22 where
 
+import Control.Monad (foldM)
+import Control.Monad.Trans.State (State, evalState, get)
 import Data.Bifunctor (Bifunctor (..))
+import Data.List (partition)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (isJust, mapMaybe)
@@ -10,14 +13,11 @@ import Debug.Trace (traceShow)
 import MyLib
 import Text.Megaparsec (parseMaybe, parseTest, sepBy)
 import Text.Megaparsec.Char
-import Control.Monad.Trans.State (State, get, evalState)
-import Data.List (partition)
-import Control.Monad (foldM)
 
 type Brick = Vec (S (S (S Z))) (Int, Int)
 
 data GameState = G
-  { _stabilized :: Map Brick (Set Brick),
+  { _support :: Map Brick (Set Brick),
     _depend :: Map Brick (Set Brick),
     _free :: Set Brick
   }
@@ -33,18 +33,19 @@ parseBlock = do
 runG :: Int -> GameState -> GameState
 runG floor g = case Set.minView (_free g) of
   Nothing -> g
-  Just (x, xs) | traceShow (length (_free g), length (_stabilized g)) True -> runG floor (dropFree floor x (g {_free = xs}))
+  -- Just (x, xs) | traceShow (length (_free g), length (_support g)) True -> runG floor (dropFree floor x (g {_free = xs}))
+  Just (x, xs) -> runG floor (dropFree floor x (g {_free = xs}))
 
 dropFree :: Int -> Brick -> GameState -> GameState
 dropFree floor b g
   | fst (vRead b' 0) <= floor =
       g
-        { _stabilized = stabilized,
+        { _support = support,
           _depend = Map.insert b (Set.singleton $ pure (0, 0)) $ _depend g
         }
   | not $ Set.null depend =
       g'
-        { _stabilized =
+        { _support =
             Set.foldl'
               ( \acc x ->
                   Map.insertWith
@@ -53,36 +54,52 @@ dropFree floor b g
                     (Set.singleton b)
                     acc
               )
-              stabilized
+              support
               depend
         }
   | otherwise = dropFree floor b' g
   where
     b' = vModify b 0 (bimap (subtract 1) (subtract 1))
-    depend = Set.filter (isJust . overlapEucVec b') (Map.keysSet $ _stabilized g)
-    stabilized = Map.insert b Set.empty (_stabilized g)
+    depend = Set.filter (isJust . overlapEucVec b') (Map.keysSet $ _support g)
+    support = Map.insert b Set.empty (_support g)
     g' = g {_depend = Map.insert b depend (_depend g)}
 
 removable :: GameState -> Brick -> Bool
-removable g b = all ((> 1) . length . (_depend g Map.!)) (_stabilized g Map.! b)
+removable g b = all ((> 1) . length . (_depend g Map.!)) (_support g Map.! b)
 
-disintegrate :: GameState -> Brick -> State (Map Brick (Set Brick)) (Set Brick)
-disintegrate g b = get >>= \m -> maybe f return (m Map.!? b)
+disintegrate :: GameState -> Brick -> Set Brick
+disintegrate g = f Set.empty g . Set.singleton
   where
-    g' = g {_depend = Map.map (Set.filter (/= b)) $ _depend g}
-    f = do
-      let x = Set.filter (null . (_depend g' Map.!)) (_stabilized g Map.! b)
-          g'' = g' {_depend = Map.map (Set.filter (`Set.notMember` x)) $ _depend g'}
-      s <- foldM (\acc b' -> (acc <>) <$> disintegrate g'' b') Set.empty x
-      return (x <> s)
+    f acc gs bs
+      | Set.null bs = acc
+      | otherwise = f acc' gs' bs'
+      where
+        (bs', depend') =
+          first Map.keysSet
+            . Map.mapEither
+              ( \x ->
+                  let x' = x Set.\\ bs
+                   in if Set.null x' then Left x' else Right x'
+              )
+            $ _depend gs
+        gs' = gs {_depend = depend'}
+        acc' = Set.union acc bs'
 
 day22 :: IO ()
 day22 = do
-  input <- lines <$> readFile "input/test22.txt"
-  -- input <- lines <$> readFile "input/input22.txt"
+  -- input <- lines <$> readFile "input/test22.txt"
+  input <- lines <$> readFile "input/input22.txt"
   let initG = G Map.empty Map.empty (Set.fromList (mapMaybe (parseMaybe parseBlock) input))
       g = runG 0 initG
-      (day22a, day22b) = partition (removable g) (Map.keys (_stabilized g))
-      n = map length $ evalState (mapM (disintegrate g) day22b) Map.empty
-  print $ length day22a
-  print $ sum n
+      (day22a, day22b) = partition (removable g) (Map.keys (_support g))
+  putStrLn
+    . ("day22a: " ++)
+    . show
+    $ length day22a
+  putStrLn
+    . ("day22b: " ++)
+    . show
+    . sum
+    $ map (length . disintegrate g) day22b
+
+-- print $ sum n
