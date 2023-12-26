330466eda12c92fdc6d4fa0ca66c17dd03183336
alex800121 <alex800121@hotmail.com>
2023-12-26 19:57:46 +0800

    day24a

--- src/Day23.hs
+++ src/Day23.hs
@@ -7,9 +7,10 @@ import qualified Data.Map as Map
 import Data.Maybe (mapMaybe)
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Debug.Trace
 import MyLib
 
-type M = Map Index (Map Index [Int])
+type M = Map Index (Map Index Int)
 
 type A = UArray Index Char
 
@@ -28,7 +29,14 @@ dirToChar West = '<'
 dirToChar East = '>'
 
 buildM :: A -> Index -> Index -> M
-buildM a end start = Map.singleton start $ f Set.empty initStarts 1 Map.empty
+buildM a end start
+  | Map.null nexts || start == end = Map.empty
+  | otherwise = Map.unionsWith Map.union (Map.singleton start nexts : map (buildM a end) (Map.keys nexts))
+  where
+    nexts = buildMFrom a end start
+
+buildMFrom :: A -> Index -> Index -> Map Index Int
+buildMFrom a end start = f (Set.singleton start) initStarts 1 Map.empty
   where
     b = bounds a
     initStarts =
@@ -40,11 +48,12 @@ buildM a end start = Map.singleton start $ f Set.empty initStarts 1 Map.empty
                   x = bimap (+ fst start) (+ snd start) i
                   c' = a ! x
                in if inRange b x && (c' `elem` [c, '.'])
-                    then Just i
+                    then Just x
                     else Nothing
           )
           [minBound .. maxBound]
     f visited starts i acc
+      -- \| traceShow starts False = undefined
       | Set.null starts = acc
       | otherwise = f visited' starts' (i + 1) acc'
       where
@@ -56,11 +65,42 @@ buildM a end start = Map.singleton start $ f Set.empty initStarts 1 Map.empty
             . Set.unions
             $ map (\(x, y) -> Set.map (bimap (+ x) (+ y)) start') adjacent
         interest' = Set.map (\k -> bimap (+ fst k) (+ snd k) (toIndex $ charToDir $ a ! k)) interest
-        acc' = Map.unionWith (<>) (Map.fromSet (const [i + 2]) interest') $ Map.unionWith (<>) (Map.fromSet (const [i]) ended) acc
+        acc' = Map.union (Map.fromSet (const (i + 2)) interest') $ Map.union (Map.fromSet (const i) ended) acc
 
 adjacent = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+calcDist :: M -> Index -> Index -> [Int]
+calcDist m end start
+  | end == start = return 0
+  | otherwise = do
+      (next, dist) <- Map.toList $ m Map.! start
+      let m' = Map.map (Map.delete start) $ Map.delete start m
+      (dist +) <$> calcDist m' end next
+
+expandM :: M -> M
+expandM =
+  Map.foldlWithKey'
+    ( \a k b ->
+        Map.unionWith Map.union (Map.map (Map.singleton k) b) $ Map.insertWith Map.union k b a
+    )
+    Map.empty
 
 day23 :: IO ()
 day23 = do
   input <- drawArray @UArray . lines <$> readFile "input/input23.txt"
-  return ()
+  -- input <- drawArray @UArray . lines <$> readFile "input/test23.txt"
+  let b = bounds input
+      start = (fst (fst b) + 1, snd (fst b))
+      end = (fst (snd b) - 1, snd (snd b))
+      m = buildM input end start
+      expanded = expandM m
+  putStrLn
+    . ("day23a: " ++)
+    . show
+    . maximum
+    $ calcDist m end start
+  putStrLn
+    . ("day23b: " ++)
+    . show
+    . maximum
+    $ calcDist expanded end start
