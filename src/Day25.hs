e0c03c5a58d71355a00d032c203af5b3512e19bd
alex800121 <alex800121@hotmail.com>
2023-12-28 06:52:52 +0800

    day25

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,9 +1,29 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day25 where
 
+import Data.Graph
+import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.List (sort, group)
+
+readInput :: String -> (Graph, Vertex -> (Int, String, [String]), String -> Maybe Vertex)
+readInput s = graphFromEdges s'
+  where
+    s' =
+      zipWith (\x (y, z) -> (x, y, z)) [0 ..]
+        . Map.toList
+        . Map.unionsWith (<>)
+        . map f
+        $ lines s
+    f x = Map.fromList $ map (,[a]) bs ++ [(a, bs)]
+      where
+        [a, b] = splitOn ": " x
+        bs = words b
 
 day25 :: IO ()
 day25 = do
-  -- input <- readFile "input/input25.txt"
-  return ()
+  -- (g, nodeFromVertex, vertexFromKey) <- readInput <$> readFile "input/input25.txt"
+  (g, nodeFromVertex, vertexFromKey) <- readInput <$> readFile "input/test25.txt"
+  print $ scc g
