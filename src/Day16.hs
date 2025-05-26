17b721527b5baf34d5dc372e36c7cd264e248895
alex800121 <alex800121@hotmail.com>
2025-05-26 10:38:19 +0800

    fast day16

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,107 +1,132 @@
 module Day16 where
 
+import Control.Monad (unless)
+import Control.Monad.ST.Strict (ST, runST)
 import Control.Parallel.Strategies (parMap, rpar)
 import Data.Array.IArray qualified as A
-import Data.Array.Unboxed (Array)
+import Data.Array.MArray qualified as M
+import Data.Array.ST (STUArray)
+import Data.Array.Unboxed (UArray)
 import Data.Bifunctor (Bifunctor (..))
-import Data.IntMap.Strict (IntMap)
-import Data.IntMap.Strict qualified as IM
-import Data.IntSet (IntSet)
-import Data.IntSet qualified as IS
-import Data.List (foldl')
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Bits (Bits (..))
+import Data.Ix
+import Data.Monoid (Sum)
+import Data.Word
+import Debug.Trace (traceM)
 import MyLib (Direction (..), drawArray)
 import Paths_AOC2023
 
 type Index = (Int, Int)
 
-type Beam = (Index, Direction)
+type Beam = (Direction, Index)
 
-type XYMap = (IntMap (IntMap Char), IntMap (IntMap Char))
+type XYMap = UArray (Direction, Index) Int
 
-buildXYMap m = (b, (xmap, ymap))
+type Visited s = STUArray s Index Word8
+type Output s = STUArray s Index Bool
+type Input = UArray Index Char
+
+-- 0 -> end
+-- 1 -> succ -> NS=/ WE=\
+-- 2 -> pred -> NS=\ WE=/
+-- 3 -> - or |
+
+-- >>> 2 ^ 8
+-- 256
+
+fromInt x = ((x `shiftR` 10, (x `shiftR` 2) .&. (2 ^ 8 - 1)), x .&. 3)
+
+toInt ((x, y), c) = (x `shiftL` 10) + (y `shiftL` 2) + c
+
+buildXYMap :: Input -> XYMap
+buildXYMap input = A.array (bimap (minBound,) (maxBound,) b) l
   where
-    b = A.bounds m
-    xmap =
-      IM.fromListWith
-        (<>)
-        [ (x, IS.singleton y)
-          | ((x, y), c) <- A.assocs m,
-            c `notElem` ".|"
-        ]
-    ymap =
-      IM.fromListWith
-        (<>)
-        [ (y, IS.singleton x)
-          | ((x, y), c) <- A.assocs m,
-            c `notElem` ".-"
-        ]
-
-toIntD lenY ((x, y), d) = (x * lenY + y) * 4 + fromEnum d
-
-toInt lenY (x, y) = x * lenY + y
-
-choices m (i, d) = maybe [] (`reflect` d) (m A.!? i)
-skipWalk (xmap, ymap) m start = IS.size $ go IS.empty IS.empty [start]
-  where
-    b@((minx, miny), (maxx, maxy)) = A.bounds m
-    lenY = maxy - miny + 1
-    go _ !acc [] = acc
-    go visited !acc (beam@(i@(x, y), d) : xs)
-      | intx `IS.member` visited = go visited' acc xs
-      | otherwise = go visited' acc' xs'
+    b@((minx, miny), (maxx, maxy)) = A.bounds input
+    l =
+      [ o
+        | (d, i, limit, j, sp, x) <- ds,
+          o <- f d i limit j sp x
+      ]
+    ds =
+      [(North, miny, maxy, (j,), succ, ((j, miny), 0)) | j <- [minx .. maxx]]
+        <> [(South, maxy, miny, (j,), pred, ((j, maxy), 0)) | j <- [minx .. maxx]]
+        <> [(East, maxx, minx, (,j), pred, ((maxx, j), 0)) | j <- [miny .. maxy]]
+        <> [(West, minx, maxx, (,j), succ, ((minx, j), 0)) | j <- [miny .. maxy]]
+    f :: Direction -> Int -> Int -> (Int -> Index) -> (Int -> Int) -> (Index, Int) -> [((Direction, Index), Int)]
+    f d i limit j sp x
+      | i == limit = [((d, i'), toInt x)]
+      | otherwise = ((d, i'), toInt x) : f d (sp i) limit j sp x'
       where
-        intx = toIntD lenY beam
-        visited' = IS.insert intx visited
-        (acc', xs') = foldl' f (acc, xs) (choices m beam)
-        f (acc, xs) d = case d of
-          North -> g acc xs d (x, miny) (second . const) (IS.maxView . fst) xmap y x
-          South -> g acc xs d (x, maxy) (second . const) (IS.minView . snd) xmap y x
-          West -> g acc xs d (minx, y) (first . const) (IS.maxView . fst) ymap x y
-          East -> g acc xs d (maxx, y) (first . const) (IS.minView . snd) ymap x y
-        g acc xs d a fb mm xymap xy0 xy1 =
-          maybe
-            (buildPoints lenY (i, a) <> acc, xs)
-            (\(xy, _) -> (buildPoints lenY (i, fb xy (x, y)) <> acc, (fb xy (x, y), d) : xs))
-            (mm . IS.split xy0 =<< (xymap IM.!? xy1))
+        i' = j i
+        x'
+          | d == North || d == South = case input A.! i' of
+              '\\' -> (i', 2)
+              '/' -> (i', 1)
+              '-' -> (i', 3)
+              _ -> x
+          | otherwise = case input A.! i' of
+              '\\' -> (i', 1)
+              '/' -> (i', 2)
+              '|' -> (i', 3)
+              _ -> x
 
-buildPoints lenY ((x0, y0), (x1, y1)) = IS.fromList [toInt lenY (x, y) | x <- xs, y <- ys]
+reflect :: XYMap -> Beam -> (Index, [Direction])
+reflect xymap b@(d, x) = case fromInt (xymap A.! b) of
+  (i, 0) -> (i, [])
+  (i, 1) -> (i, [succ d])
+  (i, 2) -> (i, [pred d])
+  (i, 3) -> (i, [succ d, pred d])
+
+getStarts :: Input -> [Beam]
+getStarts a =
+  [(d, (j, i)) | (d, i) <- [(South, miny), (North, maxy)], j <- [minx .. maxx]]
+    <> [(d, (i, j)) | (d, i) <- [(East, minx), (West, maxx)], j <- [miny .. maxy]]
   where
-    xs = [min x0 x1 .. max x0 x1]
-    ys = [min y0 y1 .. max y0 y1]
+    ((minx, miny), (maxx, maxy)) = A.bounds a
 
-reflect :: Char -> Direction -> [Direction]
-reflect '\\' d = if odd (fromEnum d) then [succ d] else [pred d]
-reflect '/' d = if odd (fromEnum d) then [pred d] else [succ d]
-reflect '|' d | odd (fromEnum d) = [pred d, succ d]
-reflect '-' d | even (fromEnum d) = [succ d, pred d]
-reflect _ d = [d]
-
-getStarts m =
-  [((x, y), d) | y <- [miny .. maxy], (x, d) <- [(minx, East), (maxx, West)]]
-    <> [((x, y), d) | x <- [minx .. maxx], (y, d) <- [(miny, South), (maxy, North)]]
+calcBeam :: Input -> XYMap -> Beam -> Int
+calcBeam input xymap b = runST $ do
+  visited <- M.newArray b' 0
+  output <- M.newArray b' False
+  mapM_ (go output visited) $ cReflect input b
+  length . filter id <$> M.getElems output
   where
-    ((minx, miny), (maxx, maxy)) = A.bounds m
+    b' = bimap snd snd (A.bounds xymap)
+    go :: Output s -> Visited s -> Beam -> ST s ()
+    go output visited b@(d, i) = do
+      v <- M.readArray visited i
+      unless (v `testBit` di) $ do
+        M.modifyArray visited i (`setBit` di)
+        mapM_ (\p -> M.writeArray output p True) path
+        mapM_ (\d' -> go output visited (d', j)) ds
+      where
+        di = fromEnum d
+        (j, ds) = reflect xymap b
+        path = A.range (min i j, max i j)
 
-reflect' '/' (dx, dy) | dy == 0 = [(dy, -dx)]
-reflect' '/' (dx, dy) | dx == 0 = [(-dy, dx)]
-reflect' '\\' (dx, dy) = [(dy, dx)]
-reflect' '|' (dx, dy) | dy == 0 = [(0, 1), (0, -1)]
-reflect' '-' (dx, dy) | dx == 0 = [(1, 0), (-1, 0)]
-reflect' _ d = [d]
+cReflect :: UArray Index Char -> Beam -> [Beam]
+cReflect a (d, i) = [(d', i) | d' <- ds]
+  where
+    ds = case a A.! i of
+      '\\' | d == South || d == North -> [pred d]
+      '\\' -> [succ d]
+      '/' | d == South || d == North -> [succ d]
+      '/' -> [pred d]
+      '-' | d == South || d == North -> [succ d, pred d]
+      '|' | d == East || d == West -> [succ d, pred d]
+      _ -> [d]
 
 day16 :: IO ()
 day16 = do
-  input <- drawArray @Array . lines <$> (getDataDir >>= readFile . (++ "/input/input16.txt"))
-  -- input <- drawArray @Array . lines <$> (getDataDir >>= readFile . (++ "/input/test16.txt"))
-  let (b, xymap) = buildXYMap input
+  input <- drawArray @UArray . lines <$> (getDataDir >>= readFile . (++ "/input/input16.txt"))
+  let xymap = buildXYMap input
   putStrLn
     . ("day16a: " ++)
     . show
-    $ skipWalk xymap input ((0, 0), East)
+    $ calcBeam input xymap (East, (0, 0))
   putStrLn
     . ("day16b: " ++)
     . show
     . maximum
-    . parMap rpar (skipWalk xymap input)
+    . parMap rpar (calcBeam input xymap)
     $ getStarts input
